180b144bd7d23512382e003c6e97de4eba1df8c6
TuiMao233 <wwu710632@gmail.com>
2020-11-28 12:07:02 +0800

    fix：修复测试文件丢失。

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 const path = require('path');
 module.exports = {
   mode: 'production',
-  entry: './lib/index.ts',
-  // entry: './lib/extends/draw-qr-code/index.ts',
+  // entry: './lib/index.ts',
+  entry: './lib/extends/draw-qr-code/index.ts',
   output: {
     filename: 'index.js',
     // 打包扩展
-    path: path.resolve(__dirname, 'dist'),
+    // path: path.resolve(__dirname, 'dist'),
     // path: path.resolve(__dirname, 'dist/extends/draw-qr-code'),
     // 打包测试
-    // path: path.resolve(__dirname, 'test/template/js_sdk/u-draw-poster'),
-    // path: path.resolve(__dirname, 'test/template/js_sdk/u-draw-poster/extends/draw-qr-code'),
+    // path: path.resolve(__dirname, 'tests/template/js_sdk/u-draw-poster'),
+    path: path.resolve(__dirname, 'tests/template/js_sdk/u-draw-poster/extends/draw-qr-code'),
     libraryTarget: "commonjs", // 采用通用模块定义
   },
   module: {

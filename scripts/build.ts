154699ca2060666832a31ee19eba9d3103b2d04b
mrmao <wwu710632@gmail.com>
2022-06-28 14:07:08 +0800

    chore: update build script

---
+++ scripts/build.ts
@@ -0,0 +1,30 @@
+import execa from 'execa'
+import consola from 'consola'
+import fs from 'fs-extra'
+import assert from 'assert'
+import { archiver, copy } from './utils'
+
+assert(process.cwd() !== __dirname)
+
+async function cli() {
+  consola.info('build:clean')
+  execa.sync('rimraf', ['dist-uniapp', 'dist-core'])
+
+  fs.mkdirpSync('dist-uniapp')
+  fs.mkdirpSync('dist-core')
+
+  consola.info('build:core')
+  execa.sync('pnpm -r --filter uni-draw-poster build', { stdio: 'inherit' })
+  execa.sync('rimraf packages/**/dist/node_modules', { stdio: 'inherit' })
+
+  consola.info('build:copy')
+  await copy('packages/uni-draw-poster/dist', 'dist-core')
+  await copy('packages/uni-draw-poster/dist', 'packages/uni-vue2-cli/js_sdk')
+  await copy('README.md', 'dist-uniapp/readme.md')
+
+  consola.info('build:zip')
+  archiver('packages/uni-vue2-cli', 'dist-uniapp/example.zip')
+  archiver('packages/uni-draw-poster/dist', 'dist-uniapp/plugin.zip')
+}
+
+if (require.main === module) cli()
---
+++ scripts/utils.ts
@@ -0,0 +1,46 @@
+import path from 'path'
+import fs from 'fs-extra'
+import consola from 'consola'
+import ar from 'archiver'
+
+const root = path.resolve(__dirname, '..')
+
+export function archiver(src: string, disc: string) {
+  src = path.join(root, src)
+  disc = path.join(root, disc)
+  // 创建文件输出流
+  const output = fs.createWriteStream(disc)
+  const archive = ar('zip', {
+    // 设置压缩级别
+    zlib: { level: 9 }
+  })
+  output.on('close', () => {
+    consola.info(`总共 ${archive.pointer()} 字节`)
+    consola.success('archiver完成文件的归档，文件输出流描述符已关闭')
+  })
+  output.on('end', () => {
+    consola.warn('数据源已耗尽')
+  })
+  archive.on('warning', (err) => {
+    if (err.code === 'ENOENT') {
+      consola.warn('stat故障和其他非阻塞错误')
+    } else {
+      throw err
+    }
+  })
+  archive.on('error', (err) => {
+    throw err
+  })
+  // 从子目录追加文件并将其命名为“新子dir”在存档中
+  archive.directory(src, false)
+  // 通过管道方法将输出流存档到文件
+  archive.pipe(output)
+  // 完成归档
+  archive.finalize()
+}
+
+export function copy(src: string, disc: string) {
+  src = path.join(root, src)
+  disc = path.join(root, disc)
+  return fs.copy(src, disc)
+}
